614ad201f5e580dfa3ad872fcad9b856bf50069b
sucnsc <584156664@qq.com>
2021-04-24 16:23:14 +0800

    pie

--- Crawler.py
+++ Crawler.py
@@ -2,6 +2,7 @@ import requests
 import UrlList
 import time, os
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
 
 requests.adapters.DEFAULT_RETRIES = 5
 
@@ -17,6 +18,7 @@ class Crawler():
         # elif isinstance(url, list):
         #     self.urls.extend(url)
         self.url = "https://arxiv.org/list/" + major + "/"
+        os.makedirs("../arxiv/" + major, exist_ok=True)
 
     # def download(self):
     #     for url in self.urls:
@@ -61,10 +63,65 @@ class Crawler():
         :param year: (str) e.g.: "21"表示2021
         :return: None
         """
-        os.makedirs("../arxiv/cs/", exist_ok=True)
-        for m in range(1, 13):
+        for m in range(1, 3):
             month = str(m) if m >= 10 else "0" + str(m)
             self.getMonthSubject(year, month)
+
+    def getMonthSubjectProp(self, year, month):
+        """
+        获取某年某月所有的论文主题的占比
+        :param year: (str) e.g.: "21"表示2021年
+        :param month: (srt) e.g.: "08"表示8月
+        :return:
+        """
+        with open("../arxiv/cs/" + year + month + ".txt") as f:
+            lines = f.readlines()
+        sum = len(lines)  # 总的主题数
+        subjectDict = {}
+        for line in lines:
+            subject = line.split(";")[-1].strip()
+            subjectDict[subject] = subjectDict.get(subject, 0) + 1
+        return self.getLabelsAndNum(subjectDict, sum)
+
+    def getYearSubjectProp(self, year):
+        """
+        获取某年所有的论文主题的占比
+        :param year: (str) e.g.: "21"表示2021年
+        :return:
+        """
+        subjectDict = {}
+        sum = 0
+        for m in range(1, 13):
+            month = str(m) if m >= 10 else "0" + str(m)
+            with open("../arxiv/cs/" + year + month + ".txt") as f:
+                lines = f.readlines()
+            sum += len(lines)
+            for line in lines:
+                subject = line.split(";")[-1].strip()
+                subjectDict[subject] = subjectDict.get(subject, 0) + 1
+        return self.getLabelsAndNum(subjectDict, sum)
+
+    def getLabelsAndNum(self, subjectDict, sum):
+        """
+        获取画圆饼图所需的主题标签和每个主题对应的占比
+        :param subjectDict: (dict[str:int])　存放主题和对应数量
+        :param sum: (int) 主题总量
+        :return: nums:(list[int])
+                 labels:(list[str])
+        """
+        subjectNum = sorted(subjectDict.items(), key=lambda a: -a[1])  # 根据主题数量逆序排序
+        labels = []
+        nums = []
+        cur = 0
+        for label, num in subjectNum:
+            labels.append(label)
+            nums.append(num)
+            cur += num
+            if sum - cur < num or num / sum < 0.03:  # 剩下的主题单个占比过少，全部归为Others
+                labels.append("Others")
+                nums.append(sum - cur)
+                break
+        return nums, labels
 
 
 if __name__ == "__main__":
@@ -73,7 +130,7 @@ if __name__ == "__main__":
         os.mkdir("../arxiv")
     crawler = Crawler("cs")
     crawler.getYearSubject("20")
-    # url = UrlList.getUrlList("https://arxiv.org/list/cs.AI/recent")
-    # print(url)
-    # crawler = Crawler(url)
-    # crawler.download()
+    #nums,labels=crawler.getMonthSubjectProp("20","03")
+    nums, labels = crawler.getYearSubjectProp("20")
+    plt.pie(nums, labels=labels, autopct="%.2f%%")
+    plt.show()
